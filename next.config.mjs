6b5476fadcfa0e7e727f3f305575cea365dd52ed
jin <a11718trannhuy@gmail.com>
2024-10-15 20:58:11 +0700

    config project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dashboard',
+                permanent: true
+            }
+        ]
+    }
+};
 
 export default nextConfig;
