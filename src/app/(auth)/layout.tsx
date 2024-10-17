27d50ff2d537b06ae8aada84b5fb6c8a43595f2c
jin <a11718trannhuy@gmail.com>
2024-10-17 20:31:25 +0700

    update source

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Authentication",
+};
+
+export default function AuthLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
